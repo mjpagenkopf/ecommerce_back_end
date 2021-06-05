a09f0283c94d3a65222441ecdb1fcb911483cece
mjpagenkopf <78058404+mjpagenkopf@users.noreply.github.com>
2021-06-05 11:57:15 -0400

    updated models

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,6 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
+const { truncate } = require('../config/connection');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -9,7 +10,36 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true, 
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(7,2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isInt: true
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+      },
+    },
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -4,13 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+}),
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+}),
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: "product_tag",
+  foreignKey: "product_id",
+  as: "tags"
+}),
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: "product_tag",
+  foreignKey: "tag_id",
+  as: "products"
+}),
 
 module.exports = {
   Product,
